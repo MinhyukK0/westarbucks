b9adab33b8a4f4b94d638efd54d38d0996a46a68
minhyukko <komhgogo@gmail.com>
2021-11-10 16:37:56 +0900

    westarbucks models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Menu, Categories, Products, Nutritions, Allergies, Product_allergies
+
+admin.site.register(Menu)
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(Nutritions)
+admin.site.register(Allergies)
+admin.site.register(Product_allergies)
