5cef0244d1c8705cbf5ab4788d7d781a9536e9ba
minhyukko <komhgogo@gmail.com>
2021-11-10 16:15:44 +0900

    meta class added

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length = 20)
     
+    class Meta:
+        db_table = 'menu'
+
     def __str__(self):
         return self.name
  
@@ -11,6 +14,8 @@ class Categories(models.Model):
     menu_id  = models.ForeignKey('Menu', on_delete = models.CASCADE)
     name = models.CharField(max_length = 20)
     
+    class Meta:
+        db_table = 'categories'
     def __str__(self):
         return self.name
  
@@ -20,7 +25,10 @@ class Products(models.Model):
     en_name = models.CharField(max_length = 50)
     product_info = models.TextField()
     new_product = models.BooleanField()
-    
+
+    class Meta:
+        db_table = 'products'
+
     def __str__(self):
         return self.kr_name
  
@@ -32,13 +40,17 @@ class Nutritions(models.Model):
     total_Carbohydrates = models.FloatField(default = 0)
     protein = models.IntegerField(default = 0)
     caffeine = models.IntegerField(default = 0)
-
+    class Meta:
+        db_table = 'nutritions'
 class Allergies(models.Model):
     name = models.CharField(max_length=20)
-    
+    class Meta:
+        db_table = 'allergies'
     def __str__(self):
         return self.name
  
 class Product_allergies(models.Model):
     product_id = models.ForeignKey('Products',on_delete = models.CASCADE)
     allergy_id = models.ForeignKey('Allergies', on_delete = models.CASCADE)
+    class Meta:
+        db_table = 'product_allergies'
